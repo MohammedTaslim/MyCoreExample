27d2fdd5be206072df4bc4f744fc3a66a57dbe62
Mohammad Taslim <mohammed.taslim@gmail.com>
2022-10-05 20:56:10 +0530

    first commit

---
+++ coreJavaTest/src/com/java/basic/MyJava.java
@@ -0,0 +1,12 @@
+package com.java.basic;
+
+public class MyJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Print hello baby");		
+
+	}
+
+}
---
+++ coreJavaTest/src/module-info.java
@@ -0,0 +1,2 @@
+module coreJavaTest {
+}
